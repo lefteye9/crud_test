d0f3000fbdd14d3832de64eb16715ff2679a1897
ryo <right_hand9@hotmail.com>
2020-10-08 20:21:41 +0900

    first_commit

---
+++ crud_test.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php:
+
+$input_a = $_POST['input_a'];
+$input_b = $_POST['input_b'];
+
+switch ( $_POST['$i'] ):
+    case create:
+        $input_a = "create";
+        var_dump($input_a);
+        break;
+    
+    case reform:
+        $input_a = "reform";
+        break;
+    case delete:
+        $input_a = "delete";
+        break;
+
+    endswitch;
+
+if($_POST['$i']){
+    
+};
+
+
+
+// if($_POST['create']){
+//     $input_a ="create";
+// }
+
+// if($_POST['reform']){
+//     $input_a ="reform";
+// }
+
+// if($_POST['delete']){
+//     $input_a ="delete";
+// }
+?>
+
+<form action="" method="post">
+<input type="text" name="input_a" id="">
+<input type="text" name="input_b" id="">
+
+<input type="submit" name="create" value="作成">
+<input type="submit" name="reform" value="更新">
+<input type="submit" name="delete" value="削除">
+</form>
+
+<?php echo $input_a; ?>
+<?php echo $input_b; ?>
+<?php echo $i; ?>
+
+    
+</body>
+</html>
